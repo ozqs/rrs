0393e10d3b1efe2a954ea40d7cca85c800deda49
ozqs <gh-ozqs@hotmail.com>
2023-10-20 22:42:14 +0800

    巨型重构！合并为一个 crate

--- src/server/main.rs
+++ src/server/main.rs
@@ -1,3 +1,78 @@
-fn main() {
-    print!("Hello world!");
+use rocket::Request;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::SeekFrom;
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> String {
+    if let Ok(indexes) = std::fs::read_to_string("indexes") {
+        return indexes;
+    }
+    String::from("UKE")
+}
+
+fn get_string(file: &mut File, start: usize, seek: usize) -> String {
+    match file.seek(SeekFrom::Start(((start + 8) * 4).try_into().unwrap())) {
+        Ok(_) => (),
+        Err(_) => {return String::from("UKE");},
+    };
+    let mut tmp = vec![0u8; seek * 4];
+    let _len = match file.read(&mut tmp) {
+        Ok(d) => d,
+        Err(_) => {return String::from("UKE");},
+    };
+    let mut tmp_start = 0;
+    let mut p: Vec<char> = Vec::new();
+    loop {
+        if tmp_start + 4 > tmp.len() {break; }
+        let gg: [u8; 4] = [tmp[tmp_start], tmp[tmp_start + 1], tmp[tmp_start + 2], tmp[tmp_start + 3]];
+        p.push(char::from_u32(u32::from_be_bytes(gg)).unwrap());
+        tmp_start += 4;
+    }
+    p.iter().collect::<String>()
+}
+
+#[get("/<name>/<start>/<seek>")]
+fn search_book(name: &str, start: usize, seek: usize) -> String {
+    let mut name = String::from(name);
+    name.push_str(".txt.rstxt");
+    if name.contains('/') || name.contains('\\') {
+        return String::from("PSD"); // 你访问啥?! ()
+    }
+
+    let mut file = match File::open(name) {
+        Ok(f) => f,
+        Err(_) => {return String::from("404");},
+    };
+
+    let mut buffer = [0u8; 8];
+    let _n = file.read(&mut buffer[..]);
+    let size: usize = usize::from_be_bytes(buffer);
+
+    // Later +8 is added the size header.
+    if start + 8 > size {
+        String::from("404")
+    } else if start + seek + 8 > size {
+        get_string(&mut file, start, size - start)
+    } else {
+        get_string(&mut file, start, seek)
+    }
+}
+
+#[catch(404)]
+fn not_found(_req: &Request) -> String { 
+    format!("404")
+}
+
+#[catch(502)]
+fn bad_gateway(_req: &Request) -> String { 
+    format!("502")
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![search_book]).mount("/", routes![index])
+    .register("/", catchers![not_found])
+    .register("/", catchers![bad_gateway])
 }
--- src/text_converter/main.rs
+++ src/text_converter/main.rs
@@ -1,3 +1,35 @@
+use std::{fs::File, io::Write};
+
+#[allow(unused)]
 fn main() {
-    print!("Hello world!");
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        fatal("参数太少！");
+    }
+
+    let input = &args[1];
+
+    let content = std::fs::read_to_string(input)
+        .unwrap_or_else(|e| fatal("打不开文件！"));
+
+    let content: Vec<char> = content.chars().collect();
+
+    let output_path = format!("{}.rstxt", input);
+
+    let mut f = File::create(output_path.as_str())
+        .unwrap_or_else(|e| fatal("打不开将写入的文件！"));
+
+    f.write_all(&(content.len()).to_be_bytes());
+
+    for ch in content {
+        let uch = ch as u32;
+        f.write_all(&uch.to_be_bytes());
+    }
+}
+
+/// # fatal
+/// 提供一个更加友好的 panic (?)
+fn fatal(s: &str) -> ! {
+    println!("Fatal: {s}");
+    std::process::exit(1)
 }

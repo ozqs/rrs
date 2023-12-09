dbab4e9d43db39adc52a392c3ebbfdade1d5926d
ozqs <gh-ozqs@hotmail.com>
2023-12-09 23:40:58 +0800

    update

--- src/text_converter.rs
+++ src/text_converter.rs
@@ -1,10 +1,9 @@
 use rrs::Fatal;
 use rrs::MyString;
+use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 use walkdir::WalkDir;
-use std::error::Error;
-
 
 /// ## To bytes
 /// 将一个字符串的每个字符转换成四个字节后合并为一个 Vec<u8>
@@ -12,19 +11,20 @@ use std::error::Error;
 /// ```rust
 /// let s = "Hello";
 /// let d = to_bytes(s);
-/// 
+///
 /// assert_eq!(d, vec![0u8, 0u8, 0u8, 104u8, 0u8, 0u8, 0u8, 101u8, 0u8, 0u8,
 ///  0u8, 108u8, 0u8, 0u8, 0u8, 108u8, 0u8, 0u8, 0u8, 111u8,])
 /// ```
-fn to_bytes(input: &str) -> Vec<u8> {
-    let mut content = Vec::new();
-    input
-        .chars()
-        .for_each(|e| content.append(&mut Vec::from((e as u32).to_be_bytes())));
 
-    content
-}
 
+// fn to_bytes(input: &str) -> Vec<u8> {
+//     let mut content = Vec::new();
+//     input
+//         .chars()
+//         .for_each(|e| content.append(&mut Vec::from((e as u32).to_be_bytes())));
+
+//     content
+// }
 
 /// ## run
 /// 给定输入路径，输出路径，生成.rrs文件
@@ -36,7 +36,10 @@ fn run(input: &str, output: &str) {
     f.write_all(&(content.len()).to_be_bytes())
         .fatal("写入文件时发生错误");
 
-    let content = to_bytes(&content);
+    let content: Vec<u8> = content
+        .chars()
+        .flat_map(|x| (x as u32).to_be_bytes())
+        .collect();
 
     f.write_all(&content).fatal("写入文件时发生错误");
 }
